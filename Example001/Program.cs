548ea9f19def35ef0a0778a185864cadae39c20a
vasiliy <vasilniy@rambler.ru>
2022-08-05 22:25:11 +0600

    Повторил программу с лекции

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,14 +1,50 @@
 ﻿Console.Clear();
 
-int xa = 40, ya = 1,
-    xb = 1, yb = 30,
-    xc = 80, yc = 30;
+int xa = 70, ya = 1;
+int xb = 1, yb = 30;
+int xc = 80, yc = 30;
 
-Console.SetCursorPosition(xa, ya);
+/*Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
 
 Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+");
 
 Console.SetCursorPosition(xc, yc);
-Console.WriteLine("+");
+Console.WriteLine("+");*/
+
+int x = xa, y = ya;
+
+int count = 0;
+
+while (count < 100000)
+{
+    int what = new Random().Next(0, 3);
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count = count + 1;
+}
+Console.SetCursorPosition(1, 31);
+Console.Write("Нажми Enter, чтобы очистить экран");
+Console.ReadLine();
+Console.Clear();
+
+
