518aa2983d964447ab8ea18fe734cf685d4805fc
vasiliy <vasilniy@rambler.ru>
2022-08-09 23:39:02 +0600

    Решена Задача 1.

--- Sem2_methods/Program.cs
+++ Sem2_methods/Program.cs
@@ -1,2 +1,29 @@
 ﻿// Задача 1. Напишите программу, которая выводит
-//случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int FindBiggestDigit(int number)
+{
+    int ed = number % 10;
+    int dec = number / 10;
+    int max;
+
+    if (ed > dec)
+    {
+        max = ed;
+    }
+    else
+    {
+        max = dec;
+    }
+
+    return max;
+}
+
+Console.Write("Рандомное двузначное число = ");
+int randomNamber = new Random().Next(10, 100);
+Console.Write(randomNamber);
+
+int biggestDigit = FindBiggestDigit(randomNamber);
+Console.WriteLine();
+Console.WriteLine($"Большее число из этих двух это: {biggestDigit}");
+
