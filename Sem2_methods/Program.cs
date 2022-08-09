2bb19ca721b31373bfd844de46cb086901b214d2
vasiliy <vasilniy@rambler.ru>
2022-08-09 23:48:29 +0600

    Решена Задача 2.

--- Sem2_methods/Program.cs
+++ Sem2_methods/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 1. Напишите программу, которая выводит
 // случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
-
+/*
 int FindBiggestDigit(int number)
 {
     int ed = number % 10;
@@ -26,4 +26,28 @@ Console.Write(randomNamber);
 int biggestDigit = FindBiggestDigit(randomNamber);
 Console.WriteLine();
 Console.WriteLine($"Большее число из этих двух это: {biggestDigit}");
+*/
 
+// Задача 2. Напишите программу, которая выводит
+// случайное трёхзначное число и удаляет вторую цифру этого числа.
+/*
+int CutNumber(int number)
+{
+    int ed = number % 10;
+    int sot = number / 100;
+    int result = sot * 10 + ed;
+    return result;
+}
+
+Console.Write("Рандомное трехзначное число = ");
+int randomNamber = new Random().Next(100, 1000);
+Console.Write(randomNamber);
+int biggestDigit = CutNumber(randomNamber);
+Console.WriteLine();
+Console.WriteLine($"Новая версия это: {biggestDigit}");
+*/
+
+// Задача 3. Напишите программу, которая будет принимать на вход
+// два числа и выводить, является ли второе число кратным первому.
+// Если второе число не кратно числу первому, то программа выводит
+// остаток от деления.
