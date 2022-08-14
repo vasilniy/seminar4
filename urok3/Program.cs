c4cb5014c31d270d057bc8e0f18425efb5d131e6
vasiliy <vasilniy@rambler.ru>
2022-08-14 23:23:48 +0600

    Создана папка к Лекции №3

---
+++ urok3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Урок 3. Цикл в цикле for ()
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        int result = i * j;
+        Console.WriteLine($"{i} * {j} = {result}; ");
+    }
+    Console.WriteLine();
+}
