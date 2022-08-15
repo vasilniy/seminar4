8e78cbf25735a35c9ba9483f08e693e7e7c92e1b
vasiliy <vasilniy@rambler.ru>
2022-08-15 13:21:23 +0600

    Задача по сортировке наименьшего значения массива в леву часть или наибольшего, если поменять знак в условии

--- urok3/Program.cs
+++ urok3/Program.cs
@@ -35,7 +35,7 @@ void SelectionSort(int[] array)
 
         for (int j = i + 1; j < array.Length; j++)
         {
-            if (array[j] < array[minPosition]) minPosition = j;
+            if (array[j] > array[minPosition]) minPosition = j; // меняем знак > или <
         }
 
         int temporary = array[i];
