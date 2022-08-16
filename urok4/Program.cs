4700e81fdb37cb33ecf797b162fec6d44ad3d1c8
vasiliy <vasilniy@rambler.ru>
2022-08-16 23:11:16 +0600

    Задача 1. Сумма чисел от 1 до N

---
+++ urok4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1. Напишите программу, которая принимает на вход
+// число (А) и выдаёт сумму чисел от 1 до А.
+
+int FindSum(int number)
+{
+    int sum = 0;
+
+    for (int current = 1; current <= number; current++ )
+        sum += current;
+
+    return sum;
+}
+
+Console.Write("Введи число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int result = FindSum(a);
+
+Console.WriteLine($"Сумма чисел от 1 до {a} равна = {result}");
